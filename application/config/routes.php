f94b002d97754de0070c56e33a85d13716c0174e
guwela <ntwananopardon@gmail.com>
2020-07-23 13:46:50 +0200

    upload files

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+
+$route['upload'] = 'errors/error404';
+
+$route['upload'] = 'Upload';
+
---
+++ application/controllers/Errors.php
@@ -0,0 +1,20 @@
+<?php
+
+// Error controller
+// This controller is used to manage the errors (404)
+class Errors extends CI_Controller 
+{
+
+    // Main controller for the contact form
+    public function error404()
+    {
+        // Create your custom controller
+
+        // Display page
+        
+        $this->load->view('errors/error404');
+        
+    }
+}
+
+?>
